45f536839e5d7e16784c6709f2c426d3f34fafd1
cabure-swag <cabureswag@gmail.com>
2025-08-14 23:59:05 -0300

    wrap app with CartProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,10 @@
+// pages/_app.js
+import { CartProvider } from "../components/CartContext";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
+  );
 }
