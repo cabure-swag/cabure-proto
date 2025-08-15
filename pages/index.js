0f857017d8e645c39076fe973ecc4deb7011d0eb
cabure-swag <cabureswag@gmail.com>
2025-08-14 22:01:42 -0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20, fontFamily: 'sans-serif' }}>
-      <h1>Caburé Demo</h1>
-      <p>Prototipo funcionando para mostrar a los vendedores.</p>
-    </div>
-  );
-}
