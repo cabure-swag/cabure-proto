358eefd6fe21b4aae3a05af0c92535105bc0a359
cabure-swag <cabureswag@gmail.com>
2025-08-14 23:46:58 -0300

    add demo brands data

---
+++ data/brands.js
@@ -0,0 +1,7 @@
+// data/brands.js
+export const BRANDS = [
+  { id: "m1", name: "Marca 1", slug: "marca-1", logoColor: "#1e90ff" },
+  { id: "m2", name: "Marca 2", slug: "marca-2", logoColor: "#ff6b6b" },
+  { id: "m3", name: "Marca 3", slug: "marca-3", logoColor: "#28a745" },
+  { id: "m4", name: "Marca 4", slug: "marca-4", logoColor: "#f4a261" }
+];
